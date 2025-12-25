52423459daf9796a19b4dcc985d1d241e24846f1
sylneii <241819935+sylneii@users.noreply.github.com>
2025-12-25 23:21:33 +0530

    add first version of turn manager

---
+++ main/turn.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func turnManager(players int) func() int {
+	turn := 0
+	return func() int {
+		if turn == players {
+			turn = 1
+		} else {
+			turn += 1
+		}
+		return turn
+	}
+}
+
+func main() {
+	nextturn := turnManager(4)
+	fmt.Println(nextturn())
+	fmt.Println(nextturn())
+	fmt.Println(nextturn())
+	fmt.Println(nextturn())
+	fmt.Println(nextturn())
+	fmt.Println(nextturn())
+}
