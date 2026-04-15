a13b9f53a11a0ffb77c28ffb715bea128af218e5
sylneii <241819935+sylneii@users.noreply.github.com>
2026-04-15 19:41:28 +0530

    lap change

--- main/trading_cards.go
+++ main/trading_cards.go
@@ -13,20 +13,38 @@ func (sc *spiceCard) play(g *Game) {
 }
 
 type upgradeCard struct {
-	maxUpgradeLevels       int
-	remainingUpgradeLevels int
+	maxUpgradeLevels int
 }
 
-func (uc *upgradeCard) play(g *Game) {
+func (uc *upgradeCard) play(g *Game, baseSpices []spice, levels []int) {
 
+	var totalLevels int
+	for _, level := range levels {
+		totalLevels += level
+	}
+	if totalLevels > uc.maxUpgradeLevels {
+		slog.Error("more than max upgrade levels")
+		return
+	}
+
+	for i, spice := range baseSpices {
+		uc.upgradeSpice(g, spice, levels[i])
+	}
 }
 
 func (uc *upgradeCard) upgradeSpice(g *Game, s spice, levels int) {
 
 	if (s + spice(levels)) > cinnamon {
-		slog.Error("invalid move")
+		slog.Error("spice value greater than cinnamon")
 		return
 	}
 	g.AddSpice(s+spice(levels), 1)
 	g.RemoveSpice(s, 1)
+}
+
+type exchangeCard struct {
+	baseSpice        spice
+	exchangeSpice    spice
+	ratioNumerator   int64
+	ratioDenominator int64
 }
